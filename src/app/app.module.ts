5c1ff8e4272ee00ecf2218bb46e7d50ff6e1692a
Nicholas Mateus <nicholas.mateus@gmail.com>
2021-01-04 07:14:05 -0300

    Melhoria na tabela e criação componente lancamentos-pesquisa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     InputTextModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
